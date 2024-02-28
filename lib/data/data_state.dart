c7b8432d77095d884b566b52c886f64cc2852612
Jill Heske <jheske@zillion.com>
2024-02-27 20:31:41 -0500

    initial commit

---
+++ lib/data/data_state.dart
@@ -0,0 +1,30 @@
+/// Copyright (C) 2024 Jill Heske
+/// This file is part of flutter-clean-architecture-drift-retrofit.
+///
+/// flutter-clean-architecture-drift-retrofit is free software: you can redistribute it and/or modify
+/// it under the terms of the Apache License, Version 2.0, as published by
+/// the Apache Software Foundation.
+///
+/// flutter-clean-architecture-drift-retrofit is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// Apache License, Version 2.0 for more details.
+///
+/// You should have received a copy of the Apache License, Version 2.0
+/// along with flutter-clean-architecture-drift-retrofit. If not, see <https:///www.apache.org/licenses/LICENSE-2.0>.
+import 'package:dio/dio.dart';
+
+abstract class DataState<T> {
+  final T? data;
+  final DioException? error;
+
+  const DataState({this.data, this.error});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  const DataFailed(DioException error) : super(error: error);
+}
---
+++ lib/presentation/router.dart
@@ -0,0 +1,15 @@
+// Define the application router using GoRouter.
+import 'package:clean_architecture_drift_getit_bloc/presentation/screens/HomeScreen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/', // Set the initial location of the router.
+  routes: <RouteBase>[
+    // Define a list of routes.
+    // Route for the home screen.
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+  ],
+);
